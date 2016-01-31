53140af84f01b5f88534031cfb5be5ce4e4fd4e4
Dima <muakacho+github@gmail.com>
2016-01-31 17:26:23 +0300

    Create GLCanvas.captureFrame.osx.js

---
+++ src/GLCanvas.captureFrame.osx.js
@@ -0,0 +1,12 @@
+const invariant = require("invariant");
+const React = require("react-native");
+const {
+  NativeModules: { GLCanvasManager }
+} = React;
+invariant(GLCanvasManager,
+`gl-react-native: the native module is not available.
+Make sure you have properly configured it.
+See README install instructions.
+React.NativeModules.GLCanvasManager is %s`, GLCanvasManager);
+
+module.exports = (handle, config) => GLCanvasManager.capture(handle, config);
