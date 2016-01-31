fe15ec088e9085204e1bc87ee3bb86fe0fce7f20
Dima <muakacho+github@gmail.com>
2016-01-31 17:36:51 +0300

    Update Surface.js

--- src/Surface.js
+++ src/Surface.js
@@ -1,7 +1,7 @@
 const invariant = require("invariant");
 const {createSurface} = require("gl-react");
 invariant(typeof createSurface === "function", "gl-react createSurface is not a function. Check your gl-react dependency");
-const React = require("react-native");
+const React = require("react-native-desktop");
 const {
   View,
   PixelRatio
