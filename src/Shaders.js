6a7a9fed2338a372d68fef8750a5cdb2b521f11d
Dima <muakacho+github@gmail.com>
2016-01-31 17:25:51 +0300

    Delete Shaders.js

--- src/Shaders.js
+++
@@ -1,6 +0,0 @@
-const {createShaders} = require("gl-react-core");
-const { NativeModules: { RNGLContext } } = require("./React");
-
-module.exports = createShaders(function (id, shader) {
-  RNGLContext.addShader(id, shader);
-});
